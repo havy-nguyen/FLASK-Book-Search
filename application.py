769cd63e08e1f431a2459c335445b2ca27361d1b
Ha Nguyen <havyhmn@gmail.com>
2020-06-25 21:01:19 +1000

    env var database set up

--- application.py
+++ application.py
@@ -1,9 +1,13 @@
 import os
+import requests
 
 from flask import Flask, session
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+# goodread key: vfTYhqGEGd0ajlUm2JQ8A
+# goodread secret: CvaXaTGKgjtGT5SindIjfdlFYGT8kaqTZwfe5pNq7I
 
 app = Flask(__name__)
 
@@ -20,7 +24,18 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+# request = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "vfTYhqGEGd0ajlUm2JQ8A", "isbns": "9781632168146"})
+
 
 @app.route("/")
 def index():
     return "Project 1: TODO"
+
+
+@app.route("/register")
+def index():
+    return "Project 1: TODO"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
