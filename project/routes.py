a0c3af45067cc1ff7d1d5cc14f3c73bb44320d78
Ha Nguyen <havyhmn@gmail.com>
2020-06-27 17:37:50 +1000

    sign-in required

--- project/routes.py
+++ project/routes.py
@@ -10,6 +10,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/", methods=['GET', 'POST'])
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+  if current_user.is_authenticated:
+    return redirect(url_for('index'))
   form = RegistrationForm()
   if form.validate_on_submit():
     hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -22,6 +24,8 @@ def register():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+  if current_user.is_authenticated:
+    return redirect(url_for('index'))
   form = LoginForm()
   if form.validate_on_submit():
     user = User.query.filter_by(email=form.email.data).first()
@@ -33,6 +37,13 @@ def login():
   return render_template('login.html', title='Sign In', form=form)
 
 
+@app.route("/logout")
+def logout():
+  logout_user()
+  return redirect(url_for('login'))
+
+
 @app.route('/index')
+@login_required
 def index():
   return render_template("index.html", title="Book Search")
